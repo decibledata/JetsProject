ddadee44d266faf92cbc3fae575976006b38ef43
George <villegas.george96@gmail.com>
2024-11-08 13:41:01 -0800

    Stub App and Airfield classes

---
+++ src/com/skilldistillery/jets/app/AirField.java
@@ -0,0 +1,9 @@
+package com.skilldistillery.jets.app;
+
+import java.util.List;
+import com.skilldistillery.jets.entities.Jets;
+public class AirField {
+
+	private List<Jets> fleet;
+	
+}
--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -1,5 +1,26 @@
 package com.skilldistillery.jets.app;
 
+import java.util.Scanner;
+
 public class JetsApplication {
 
+	private AirField airField= new AirField();
+	private static Scanner sc;
+	
+	public static void main(String[] args) {
+		sc = new Scanner(System.in);
+		
+		sc.close();
+	}
+	
+	private void launch() {
+		//TODO Loop;
+		//Displayusermenu()
+		//get choice from scanner
+		// act on choice
+	}
+	
+	private void displayUserMenu() {
+		
+	}
 }
